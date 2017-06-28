cd1bcbe59a5dfb8759bf2aff202d8857970e50df
the-nerbs <scott.nerbetski@gmail.com>
2017-06-27 20:08:11 -0400

    Some performance tests (code + sample results) comparing the time it takes to enumerate over an Array, a List<T>, and an IList<T>.

---
+++ PerfTesting/PerfTesting/CountedTimer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfTesting
+{
+    class CountedStopwatch
+    {
+        private readonly Stopwatch _timer = new Stopwatch();
+
+        public string TestName { get; }
+        public int Intervals { get; private set; }
+
+
+        public bool IsRunning
+        {
+            get { return _timer.IsRunning; }
+        }
+
+        public TimeSpan Elapsed
+        {
+            get { return _timer.Elapsed; }
+        }
+
+        public long ElapsedMilliseconds
+        {
+            get { return _timer.ElapsedMilliseconds; }
+        }
+
+        public long ElapsedTicks
+        {
+            get { return _timer.ElapsedTicks; }
+        }
+
+        public TimeSpan AverageElapsed
+        {
+            get
+            {
+                long ticks = Elapsed.Ticks / Intervals;
+                return TimeSpan.FromTicks(ticks);
+            }
+        }
+
+
+        public CountedStopwatch(string testName)
+        {
+            TestName = testName;
+        }
+
+
+        public void Reset()
+        {
+            Intervals = 0;
+            _timer.Reset();
+        }
+
+        public void Restart()
+        {
+            Reset();
+            Start();
+        }
+
+        public void Start()
+        {
+            Intervals++;
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ PerfTesting/PerfTesting/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfTesting
+{
+    class Program
+    {
+        const int TimedIntervals = 100000;
+        const int CollectionSize = 100;
+        const int StringSize = 5;
+        const string StringContents = "abcdefghijklmnopqrstuvwxyz";
+
+        static int stringIndex = 0;
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void DontElide<T>(T value)
+        { }
+
+
+        static CountedStopwatch Time_Foreach_Array(string[] list)
+        {
+            var timer = new CountedStopwatch("Foreach_Array");
+
+            for (int i = 0; i < TimedIntervals; i++)
+            {
+                timer.Start();
+
+                foreach (var item in list)
+                {
+                    DontElide(item);
+                }
+
+                timer.Stop();
+            }
+
+            return timer;
+        }
+
+        static CountedStopwatch Time_For_Array(string[] list)
+        {
+            var timer = new CountedStopwatch("For_Array");
+
+            for (int i = 0; i < TimedIntervals; i++)
+            {
+                timer.Start();
+
+                for (int j = 0; j < list.Length; j++)
+                {
+                    var item = list[j];
+                    DontElide(item);
+                }
+
+                timer.Stop();
+            }
+
+            return timer;
+        }
+
+        static CountedStopwatch Time_Foreach_List(List<string> list)
+        {
+            var timer = new CountedStopwatch("Foreach_List");
+
+            for (int i = 0; i < TimedIntervals; i++)
+            {
+                timer.Start();
+
+                foreach (var item in list)
+                {
+                    DontElide(item);
+                }
+
+                timer.Stop();
+            }
+
+            return timer;
+        }
+
+        static CountedStopwatch Time_For_List(List<string> list)
+        {
+            var timer = new CountedStopwatch("For_List");
+
+            for (int i = 0; i < TimedIntervals; i++)
+            {
+                timer.Start();
+
+                for (int j = 0; j < list.Count; j++)
+                {
+                    var item = list[j];
+                    DontElide(item);
+                }
+
+                timer.Stop();
+            }
+
+            return timer;
+        }
+
+        static CountedStopwatch Time_Foreach_IList(IList<string> list)
+        {
+            var timer = new CountedStopwatch($"Foreach_IList ({list.GetType()})");
+
+            for (int i = 0; i < TimedIntervals; i++)
+            {
+                timer.Start();
+
+                foreach (var item in list)
+                {
+                    DontElide(item);
+                }
+
+                timer.Stop();
+            }
+
+            return timer;
+        }
+
+        static CountedStopwatch Time_For_IList(IList<string> list)
+        {
+            var timer = new CountedStopwatch($"For_IList ({list.GetType()})");
+
+            for (int i = 0; i < TimedIntervals; i++)
+            {
+                timer.Start();
+
+                for (int j = 0; j < list.Count; j++)
+                {
+                    var item = list[j];
+                    DontElide(item);
+                }
+
+                timer.Stop();
+            }
+
+            return timer;
+        }
+
+
+        static string NextString()
+        {
+            var chars = new char[StringSize];
+
+            for (int i = 0; i < StringSize; i++)
+            {
+                chars[i] = StringContents[stringIndex];
+                stringIndex = (stringIndex + 1) % StringContents.Length;
+            }
+
+            return new string(chars);
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Data setup:
+            var list = new List<string>(capacity: CollectionSize);
+
+            for (int i = 0; i < list.Capacity; i++)
+            {
+                list.Add(NextString());
+            }
+
+            var array = list.ToArray();
+
+
+            // perf counting:
+            CountedStopwatch[] times =
+            {
+                // array tests:
+                Time_Foreach_Array(array),
+                Time_For_Array(array),
+                Time_Foreach_IList(array),
+                Time_For_IList(array),
+
+                // List<T> tests:
+                Time_Foreach_List(list),
+                Time_For_List(list),
+                Time_Foreach_IList(list),
+                Time_For_IList(list),
+            };
+
+
+            // output:
+            string sepLine = new string('=', 80);
+            foreach (var result in times)
+            {
+                Console.WriteLine(sepLine);
+                Console.WriteLine("Test: " + result.TestName);
+                Console.WriteLine();
+                Console.WriteLine("# Intervals:     " + result.Intervals);
+                Console.WriteLine("Total Time:      " + result.Elapsed.TotalMilliseconds + " ms");
+                Console.WriteLine("Average Time:    " + result.AverageElapsed.TotalMilliseconds + " ms");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey(intercept: true);
+        }
+    }
+}
